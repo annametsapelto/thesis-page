40090ec6e507adfe41c1fc4c0f7f7ae12434e12a
Anna Metsäpelto <anna.metsapelto@gmail.com>
2021-10-29 11:09:44 +0300

    Add css for second text and mod ornament

--- thesis-page/src/Components/SecondText.js
+++ thesis-page/src/Components/SecondText.js
@@ -1,17 +1,18 @@
 import Ornaments from './Ornaments';
+import '../Styles/SecondText.css';
 
 const SecondText = (props) => {
     return (
-        <div>
+        <div className="second">
             <h3>My Reenactment Eras</h3>
             <Ornaments isLightMode={props.isLightMode}/>
-            <h5>The Viking Age (years 700-1100)</h5>
+            <h4>The Viking Age (years 700-1100)</h4>
             <p>My latest period is the Viking age. Lots of misconceptions about Viking clothing and life are moving around because of entertainment industry and shows like The Vikings which are rather far from real history. So I’ve been reading about grave finds and interpretations of the clothes they wore. The Viking age was so long ago that most clothes are just fragments and definite truths are hard to find but I try to stick to the best evidence.</p>
-            <h5>The Middle Ages (years 1100-1400)</h5>
+            <h4>The Middle Ages (years 1100-1400)</h4>
             <p>This is the period I started reenacting and for which I made my first garb. There is more archeological, visual and written evidence of the garb and other areas of life than from the Viking age which is one of the reasons I didn’t make any Viking garb earlier. The Middle Ages is also the era that is used as a model for lots of fantasy fiction that I’ve loved since childhood and what drew me to do reenactment. I’ve had several different garbs from this period and even got married in one.</p>
-            <h5>The Renaissance (years 1400-1600)</h5>
+            <h4>The Renaissance (years 1400-1600)</h4>
             <p>The dances in our association date from the Renaissance period Italy, France and England and I love the dances, especially the Italian ones. That is why I chose to have an Italian late 15th century name Dianora del Bianco as my alias in most of my reenacting. The garb is also fabulous though somewhat harder to make than garb from earlier periods. The art and architecture also inspire me in this period.</p>
-            <h5>Rococo (18th Century)</h5>
+            <h4>Rococo (18th Century)</h4>
             <p>I’ve participated in a couple of balls in this period but still don’t have a full set of garb. Even though clothes in this period are exquisite, they basically require the use of stays made to fit and I haven’t had the courage to start such a time-consuming project in my childbearing years when my waist doesn’t have a set size. However, I do have pieces of clothing and some fabric ready waiting for the time when I’ll make that garb.</p>
         </div>
     )
