5089d52f383243d1a5d2e7cc23520fa30ff7af5e
Anna Metsäpelto <anna.metsapelto@gmail.com>
2021-12-09 15:35:58 +0200

    Style the conditioning of the arrows better

--- thesis-page/src/Components/ArrowLeft.js
+++ thesis-page/src/Components/ArrowLeft.js
@@ -1,6 +1,39 @@
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from '@emotion/react';
+import Arrow from '../Images/arrow-left.png';
+
+
 const ArrowLeft = (props) => {
+const styles = ({isLightMode}) => css`
+            height: 90px;
+            width: 60px;
+            background-color: #EEB414; 
+            cursor: pointer;
+            min-width: 70px;
+            background-image: url(${Arrow});
+            background-repeat: no-repeat;
+            background-size: 90%;
+            margin-right: 2px;
+            &:hover {
+                background-color: #F6F3E6;
+            }
+            ${isLightMode === false &&
+            `
+                background-color: #F6F3E6;
+                &:hover {
+                    background-color: #EEB414;
+                } 
+            `}
+            @media only screen and (max-width: 1200px) {
+                width: 40px;
+                height: 60px;
+                min-width: 40px;
+            }`
+       
     return (
-        <button onClick={props.switchLeft}/>
+        <button onClick={props.switchLeft}
+            css={styles}
+        />
     )
 }
 
--- thesis-page/src/Components/ArrowRight.js
+++ thesis-page/src/Components/ArrowRight.js
@@ -1,8 +1,38 @@
-
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from '@emotion/react';
+import Arrow from '../Images/arrow-right.png';
 
 const ArrowRight = (props) => {
+    const styles = ({isLightMode}) => css`
+            height: 90px;
+            width: 60px;
+            background-color: #EEB414; 
+            cursor: pointer;
+            min-width: 70px;
+            background-image: url(${Arrow});
+            background-repeat: no-repeat;
+            background-size: 90%;
+            margin-left: 2px;
+            &:hover {
+                background-color: #F6F3E6;
+            }
+            ${isLightMode === false &&
+                `
+                    background-color: #F6F3E6;
+                    &:hover {
+                        background-color: #EEB414;
+                    } 
+                `}
+            @media only screen and (max-width: 1200px) {
+                width: 40px;
+                height: 60px;
+                min-width: 40px;
+            }`
+        
     return (
-        <button onClick={props.switchRight}/>
+        <button onClick={props.switchRight}
+            css={styles}
+        />
     )
 }
 
